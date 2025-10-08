ab7e2e84f56db21560f6171e2fe1acf8e60c89ea
Golden_Reflection <reflectiongolden@gmail.com>
2025-10-08 15:15:59 +0100

    added unshiftArray function, no button handler

--- src/project/array_manip/array-manip.js
+++ src/project/array_manip/array-manip.js
@@ -1,3 +1,25 @@
 //Array creation
 let array = ["Initial value"]
 
+//functions
+function unshiftArray(array, value) {
+    let values = [];
+
+    if (value.includes(", ")) {
+        console.log("Adding '" + values + "' as elements to the beginning of the array.")
+        values = value.split(", ")
+        for (let i = 0; i < values.length; i++) {
+            const element = values[i];
+            array.unshift(element);
+        }
+    }
+    else {
+        console.log("Adding '" + value + "' as an element to the beginning of the array.");
+        array.unshift(value);
+    }
+}
+
+//initial display
+
+
+//Button Handlers
