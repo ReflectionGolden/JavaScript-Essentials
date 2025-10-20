a64b726d4feb07b8f6211fa9cf2edbe8931300e9
Golden_Reflection <reflectiongolden@gmail.com>
2025-10-20 15:10:23 +0100

    Added javascript for the calulator

---
+++ src/project/simple_calc/simple-calc.js
@@ -0,0 +1,123 @@
+// Element Selectors
+const InputFieldOne = document.querySelector("#input1");
+const InputFieldTwo = document.querySelector("#input2");
+const ResultField   = document.querySelector("#result");
+
+const AddButton    = document.querySelector("#AddButton");
+const MultButton   = document.querySelector("#MultButton");
+const SubtButton   = document.querySelector("#SubtButton");
+const DivideButton = document.querySelector("#DivButton");
+
+//error list
+const Errors = {
+    "Error0": "Internal Error: Value somehow requested from non-existent input.",
+    "Error1": "Input Error: Please imput valid numbers.",
+    "Error2": "Input Error: Division by zero is not allowed"
+}
+
+// Functions
+function getInput(id) {
+    let inputValue;
+    switch (id) {
+        case 1:
+            inputValue = InputFieldOne.value;
+            break;
+        case 2:
+            inputValue = InputFieldTwo.value;
+            break;
+        default:
+            inputValue = "Error0";
+            return inputValue;
+    }
+    return parseFloat(inputValue);
+}
+
+function displayResult(result) {
+    if (result.includes("Error")) {
+        ResultField.textContent = Errors[result];
+    }
+    else {
+        ResultField.textContent = result;
+    }
+}
+
+function addInputs() {
+    let x = getInput(1);
+    let y = getInput(2);
+    let z;
+
+    if (x == "Error0" || y == "Error 0") {
+        z = "Error0";
+    }
+    else if (isNaN(x) || isNaN(y)) {
+        z = "Error1";
+    }
+    else {
+        z = x + y;
+    }
+
+    displayResult(z);
+}
+
+function subtractInputs() {
+    let x = getInput(1);
+    let y = getInput(2);
+    let z;
+
+    if (x == "Error0" || y == "Error 0") {
+        z = "Error0";
+    }
+    else if (isNaN(x) || isNaN(y)) {
+        z = "Error1";
+    }
+    else {
+        z = x - y;
+    }
+
+    displayResult(z);
+}
+
+function multiplyInputs() {
+    let x = getInput(1);
+    let y = getInput(2);
+    let z;
+
+    if (x == "Error0" || y == "Error 0") {
+        z = "Error0";
+    }
+    else if (isNaN(x) || isNaN(y)) {
+        z = "Error1";
+    }
+    else {
+        z = x * y;
+    }
+
+    displayResult(z);
+}
+
+function divideInputs() {
+    let x = getInput(1);
+    let y = getInput(2);
+    let z;
+
+    if (x == "Error0" || y == "Error 0") {
+        z = "Error0";
+    }
+    else if (isNaN(x) || isNaN(y)) {
+        z = "Error1";
+    }
+    else if (y == 0) {
+        z = "Error2";
+    }
+    else {
+        z = x * y;
+    }
+
+    displayResult(z);
+}
+
+// Handlers
+AddButton.addEventListener("click", addInputs);
+SubtButton.addEventListener("click", subtractInputs);
+MultButton.addEventListener("click", multiplyInputs);
+DivideButton.addEventListener("click", divideInputs);
